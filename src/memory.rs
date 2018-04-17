1fb3eeda58903386824ba06abafd0317c94df9d2
Tom de Bruijn <tom@tomdebruijn.com>
2018-04-17 17:18:22 +0200

    Use runtime metrics for container memory metrics
    Most "files" in the virtual file system `/proc/` return metrics for the
    host system rather than the container in which it is fetched. To get the
    values for the host metrics in containers we need to read the `/sys/fs/`
    virtual file system instead.
    
    This commit fixes the memory metrics reporting for containers that
    expose the `/sys/fs/` virtual file system.
    
    Note: This change does hard code the memory reporting to containers,
    this needs to be automatically detected in the future.
    
    Based on the docs as described here:
    https://docs.docker.com/config/containers/runmetrics/
    
    Something in the calculations for `/proc/meminfo` was messing up the
    memory calculations for container metrics. This change moves the
    calculations to the read functions so the implementation of the
    calculations can differ for these two reporting methods.
    
    To get accurate "used" memory, it was needed to subtract page cache from
    used memory. This also allows us to get the free memory.
    
    As implemented in `docker stats`
    https://github.com/docker/cli/blob/1401d5daf2f49a97791487dd5c5a8598907f0bf1/cli/command/container/stats_helpers.go#L226-L229

--- src/memory.rs
+++ src/memory.rs
@@ -4,10 +4,12 @@ use super::Result;
 pub struct Memory {
     total: u64,
     free: u64,
+    used: u64,
     buffers: u64,
     cached: u64,
     swap_total: u64,
     swap_free: u64,
+    swap_used: u64,
 }
 
 const MEMORY_NUMBER_OF_FIELDS: usize = 6;
@@ -18,16 +20,13 @@ impl Memory {
         self.total
     }
 
-    /// Total amount of free physical memory in Kb.
-    /// Inclused buffers and caches, these will be freed
-    /// up by the OS when the memory is needed.
     pub fn free(&self) -> u64 {
-        self.free + self.buffers + self.cached
+        self.free
     }
 
     /// Total amount of used physical memory in Kb.
     pub fn used(&self) -> u64 {
-        self.total() - self.free()
+        self.used
     }
 
     /// Total amount of swap space in Kb.
@@ -42,7 +41,7 @@ impl Memory {
 
     /// Total amount of used swap space in Kb.
     pub fn swap_used(&self) -> u64 {
-        self.swap_total() - self.swap_free()
+        self.swap_used
     }
 }
 
@@ -59,23 +58,26 @@ mod os {
 
     use super::{Memory,MEMORY_NUMBER_OF_FIELDS};
     use super::super::{ProbeError,Result};
-    use super::super::file_to_buf_reader;
+    use super::super::{file_to_buf_reader,parse_u64};
 
     #[inline]
     pub fn read() -> Result<Memory> {
-        read_and_parse_memory(&Path::new("/proc/meminfo"))
+        read_and_parse_sys_memory(&Path::new("/sys/fs/cgroup/memory/"))
     }
 
     #[inline]
-    pub fn read_and_parse_memory(path: &Path) -> Result<Memory> {
+    pub fn read_and_parse_proc_memory(path: &Path) -> Result<Memory> {
         let mut memory = Memory {
             total: 0,
             free: 0,
+            used: 0,
             buffers: 0,
             cached: 0,
             swap_total: 0,
             swap_free: 0,
+            swap_used: 0,
         };
+        let mut free = 0;
 
         let reader = try!(file_to_buf_reader(path));
 
@@ -83,9 +85,7 @@ mod os {
         for line in reader.lines() {
             let line = try!(line);
             let segments: Vec<&str> = line.split_whitespace().collect();
-            let value: u64 = try!(segments[1].parse().map_err(|_| {
-                ProbeError::UnexpectedContent(format!("Could not parse value for '{}'", segments[0]))
-            }));
+            let value: u64 = try!(parse_u64(segments[1]));
 
             // If this is a field we recognize set it's value and increment the
             // number of fields we encountered.
@@ -95,7 +95,7 @@ mod os {
                     1
                 },
                 "MemFree:" => {
-                    memory.free = value;
+                    free = value;
                     1
                 },
                 "Buffers:" => {
@@ -126,7 +126,72 @@ mod os {
             return Err(ProbeError::UnexpectedContent("Did not encounter all expected fields".to_owned()))
         }
 
+        // Total amount of free physical memory in Kb.
+        // Includes buffers and caches, these will be freed
+        // up by the OS when the memory is needed.
+        memory.free = free + memory.buffers + memory.cached;
+        memory.used = memory.total - memory.free;
+        memory.swap_used = memory.swap_total - memory.swap_free;
+
         Ok(memory)
+    }
+
+    pub fn read_and_parse_sys_memory(path: &Path) -> Result<Memory> {
+        let mut memory = Memory {
+            total: 0,
+            free: 0,
+            used: 0,
+            buffers: 0,
+            cached: 0,
+            swap_total: 0,
+            swap_free: 0,
+            swap_used: 0,
+        };
+
+        memory.total = bytes_to_kilo_bytes(try!(read_file_value_as_u64(&path.join("memory.limit_in_bytes"))));
+        let used_memory = bytes_to_kilo_bytes(try!(read_file_value_as_u64(&path.join("memory.usage_in_bytes"))));
+        // If swap is not configured for the container, read 0 as value
+        match read_file_value_as_u64(&path.join("memory.memsw.limit_in_bytes")) {
+            Ok(value) => memory.swap_total = bytes_to_kilo_bytes(value),
+            Err(_) => memory.swap_total = 0
+        }
+        let mut swap_used: Option<u64> = None;
+
+        let reader = try!(file_to_buf_reader(&path.join("memory.stat")));
+        for line in reader.lines() {
+            let line = try!(line);
+            let segments: Vec<&str> = line.split_whitespace().collect();
+            let value = try!(parse_u64(&segments[1]));
+
+            match segments[0] {
+                "swap" => swap_used = Some(bytes_to_kilo_bytes(value)),
+                "cache" => memory.cached = bytes_to_kilo_bytes(value),
+                _ => ()
+            };
+        }
+
+        memory.used = used_memory - memory.cached;
+        memory.free = memory.total - memory.used;
+        match swap_used {
+            Some(value) => {
+                memory.swap_used = value;
+                memory.swap_free = memory.swap_total - value
+            },
+            None => ()
+        }
+
+        Ok(memory)
+    }
+
+    fn read_file_value_as_u64(path: &Path) -> Result<u64> {
+        let mut reader = try!(file_to_buf_reader(path));
+        let mut line = String::new();
+        try!(reader.read_line(&mut line));
+        parse_u64(&line.trim())
+    }
+
+    fn bytes_to_kilo_bytes(bytes: u64) -> u64 {
+        bytes.checked_div(1024).unwrap_or(0)
     }
 }
 
@@ -142,25 +207,29 @@ mod tests {
     }
 
     #[test]
-    fn test_read_and_parse_memory() {
+    fn test_read_and_parse_proc_memory() {
         let path = Path::new("fixtures/linux/memory/proc_meminfo");
-        let memory = super::os::read_and_parse_memory(&path).unwrap();
+        let memory = super::os::read_and_parse_proc_memory(&path).unwrap();
 
         let expected = Memory {
             total: 376072,
-            free: 125104,
+            free: 324248,
+            used: 51824,
             buffers: 22820,
             cached: 176324,
             swap_total: 1101816,
             swap_free: 1100644,
+            swap_used: 1172,
         };
         assert_eq!(expected, memory);
+        assert_eq!(memory.total, memory.used + memory.free);
+        assert_eq!(memory.swap_total, memory.swap_used + memory.swap_free);
     }
 
     #[test]
     fn test_read_and_parse_memory_wrong_path() {
         let path = Path::new("/nonsense");
-        match super::os::read_and_parse_memory(&path) {
+        match super::os::read_and_parse_proc_memory(&path) {
             Err(ProbeError::IO(_)) => (),
             r => panic!("Unexpected result: {:?}", r)
         }
@@ -169,7 +238,7 @@ mod tests {
     #[test]
     fn test_read_and_parse_memory_incomplete() {
         let path = Path::new("fixtures/linux/memory/proc_meminfo_incomplete");
-        match super::os::read_and_parse_memory(&path) {
+        match super::os::read_and_parse_proc_memory(&path) {
             Err(ProbeError::UnexpectedContent(_)) => (),
             r => panic!("Unexpected result: {:?}", r)
         }
@@ -178,31 +247,85 @@ mod tests {
     #[test]
     fn test_read_and_parse_memory_garbage() {
         let path = Path::new("fixtures/linux/memory/proc_meminfo_garbage");
-        match super::os::read_and_parse_memory(&path) {
+        match super::os::read_and_parse_proc_memory(&path) {
             Err(ProbeError::UnexpectedContent(_)) => (),
             r => panic!("Unexpected result: {:?}", r)
         }
     }
 
     #[test]
-    fn test_memory_calculations() {
-        let memory = Memory {
-            total: 1000,
-            free: 200,
-            buffers: 100,
-            cached: 100,
-            swap_total: 500,
-            swap_free: 100,
+    fn test_read_and_parse_sys_memory() {
+        let path = Path::new("fixtures/linux/sys/fs/cgroup/memory/");
+        let memory = super::os::read_and_parse_sys_memory(&path).unwrap();
+
+        let expected = Memory {
+            total: 512000, // 500mb
+            free: 503400, // total - used
+            used: 8600,
+            buffers: 0,
+            cached: 58928,
+            swap_total: 2000,
+            swap_free: 1500,
+            swap_used: 500,
         };
+        assert_eq!(expected, memory);
+        assert_eq!(memory.total, memory.used + memory.free);
+        assert_eq!(memory.swap_total, memory.swap_used + memory.swap_free);
+    }
 
-        // Physical memory
-        assert_eq!(1000, memory.total());
-        assert_eq!(400, memory.free());
-        assert_eq!(600, memory.used());
+    #[test]
+    fn test_read_and_parse_sys_memory_wrong_path() {
+        let path = Path::new("/nonsense");
+        match super::os::read_and_parse_sys_memory(&path) {
+            Err(ProbeError::IO(_)) => (),
+            r => panic!("Unexpected result: {:?}", r)
+        }
+    }
 
-        // Swap space
-        assert_eq!(500, memory.swap_total());
-        assert_eq!(100, memory.swap_free());
-        assert_eq!(400, memory.swap_used());
+    #[test]
+    fn test_read_and_parse_sys_memory_incomplete() {
+        let path = Path::new("fixtures/linux/sys/fs/cgroup/memory_incomplete/");
+        match super::os::read_and_parse_sys_memory(&path) {
+            Err(ProbeError::UnexpectedContent(_)) => (),
+            r => panic!("Unexpected result: {:?}", r)
+        }
+    }
+
+    #[test]
+    fn test_read_and_parse_sys_memory_missing_files() {
+        let path = Path::new("fixtures/linux/sys/fs/cgroup/memory_missing_files/");
+        match super::os::read_and_parse_sys_memory(&path) {
+            Err(ProbeError::IO(_)) => (),
+            r => panic!("Unexpected result: {:?}", r)
+        }
+    }
+
+    #[test]
+    fn test_read_and_parse_sys_memory_garbage() {
+        let path = Path::new("fixtures/linux/sys/fs/cgroup/memory_garbage/");
+        match super::os::read_and_parse_sys_memory(&path) {
+            Err(ProbeError::UnexpectedContent(_)) => (),
+            r => panic!("Unexpected result: {:?}", r)
+        }
+    }
+
+    #[test]
+    fn test_read_and_parse_sys_memory_no_swap() {
+        let path = Path::new("fixtures/linux/sys/fs/cgroup/memory_without_swap/");
+        let memory = super::os::read_and_parse_sys_memory(&path).unwrap();
+
+        let expected = Memory {
+            total: 512000, // 500mb
+            free: 503400, // total - used
+            used: 8600,
+            buffers: 0,
+            cached: 58928,
+            swap_total: 0, // Reads 0 swap
+            swap_free: 0,  // Reads 0 swap
+            swap_used: 0,
+        };
+        assert_eq!(expected, memory);
+        assert_eq!(memory.total, memory.used + memory.free);
+        assert_eq!(memory.swap_total, memory.swap_used + memory.swap_free);
     }
 }
