bb52cb34c24552a30b54f636f71be248b748e3f1
Noemi <45180344+unflxw@users.noreply.github.com>
2023-02-14 16:38:42 +0100

    Make memory struct members public
    Instead of using getters, make the members of the memory struct
    publicly addressable.

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -5,49 +5,13 @@ pub mod proc;
 
 #[derive(Debug, PartialEq)]
 pub struct Memory {
-    total: Option<u64>,
-    free: Option<u64>,
-    used: u64,
-    buffers: Option<u64>,
-    cached: Option<u64>,
-    shmem: Option<u64>,
-    swap_total: Option<u64>,
-    swap_free: Option<u64>,
-    swap_used: Option<u64>,
-}
-
-impl Memory {
-    /// Total amount of physical memory in Kb.
-    pub fn total(&self) -> Option<u64> {
-        self.total
-    }
-
-    pub fn free(&self) -> Option<u64> {
-        self.free
-    }
-
-    /// Total amount of used physical memory in Kb.
-    pub fn used(&self) -> u64 {
-        self.used
-    }
-
-    /// Total amount of swap space in Kb.
-    pub fn swap_total(&self) -> Option<u64> {
-        self.swap_total
-    }
-
-    /// Total amount of free swap space in Kb.
-    pub fn swap_free(&self) -> Option<u64> {
-        self.swap_free
-    }
-
-    /// Total amount of used swap space in Kb.
-    pub fn swap_used(&self) -> Option<u64> {
-        self.swap_used
-    }
-
-    /// Total amount of shared memory
-    pub fn shmem(&self) -> Option<u64> {
-        self.shmem
-    }
+    pub total: Option<u64>,
+    pub free: Option<u64>,
+    pub used: u64,
+    pub buffers: Option<u64>,
+    pub cached: Option<u64>,
+    pub shmem: Option<u64>,
+    pub swap_total: Option<u64>,
+    pub swap_free: Option<u64>,
+    pub swap_used: Option<u64>,
 }
